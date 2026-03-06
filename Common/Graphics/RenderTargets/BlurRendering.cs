7f9345609064aa6fbe1762de55ce4c75004ddf08
ebonfly <73665110+ebonfly@users.noreply.github.com>
2026-03-06 23:37:55 +0100

    oops

--- Common/Graphics/RenderTargets/BlurRendering.cs
+++ Common/Graphics/RenderTargets/BlurRendering.cs
@@ -1,25 +1,26 @@
-/*namespace EbonianMod.Common.Graphics.RenderTargets;
+namespace EbonianMod.Common.Graphics.RenderTargets;
 
-public class BlurRendering : BaseCachedActionRenderTarget<BlurRendering>
+public class BlurRendering : BaseCachedActionRenderTarget<BlurRendering> // Unused 
 {
 	public override int TargetAmount => 2;
 	public override void PopulateTarget()
 	{
-		Main.graphics.GraphicsDevice.SetRenderTarget(Targets[0]);
+		DrawCache.Clear();
+		/*Main.graphics.GraphicsDevice.SetRenderTarget(Targets[0]);
 		Main.graphics.GraphicsDevice.Clear(Color.Transparent);
 
 		Main.spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, Main.DefaultSamplerState, DepthStencilState.None, RasterizerState.CullNone, null, Main.GameViewMatrix.TransformationMatrix);
 		DrawCache.InvokeAllAndClear();
-		Main.spriteBatch.End();
+		Main.spriteBatch.End();*/
 	}
 
 	public override void DrawTarget()
 	{
-		Main.spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend);
+		/*Main.spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend);
 		Effects.Test2.Value.CurrentTechnique.Passes[0].Apply();
 		Effects.Test2.Value.Parameters["tex0"].SetValue(Targets[0]);
 		Effects.Test2.Value.Parameters["i"].SetValue(0.02f);
 		Main.spriteBatch.Draw(Main.screenTarget, Vector2.Zero, Color.White);
-		Main.spriteBatch.End();
+		Main.spriteBatch.End();*/
 	}
-}*/ // Unused
+}
